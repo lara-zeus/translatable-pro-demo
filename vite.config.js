b0b4001a049544109f5069bf155d7d7ed5c4e9fe
a7medKhalid <ahmed1alghamdi2000@gmail.com>
2025-09-05 04:42:49 +0300

    tailwind 4 configs

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
     plugins: [
@@ -11,5 +12,6 @@ export default defineConfig({
             ],
             refresh: true,
         }),
+        tailwind(),
     ],
 });
