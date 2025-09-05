9991932b2ac0daeb1186ba0fdaab8ce876995189
a7medKhalid <ahmed1alghamdi2000@gmail.com>
2025-09-05 03:48:30 +0300

    upgrade

--- app/Filament/Admin/Resources/ChapterResource/Pages/ListChapters.php
+++ app/Filament/Admin/Resources/ChapterResource/Pages/ListChapters.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Admin\Resources\ChapterResource\Pages;
 
+use Filament\Actions\CreateAction;
 use App\Filament\Admin\Resources\ChapterResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
@@ -13,7 +14,7 @@ class ListChapters extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            CreateAction::make(),
         ];
     }
 }
