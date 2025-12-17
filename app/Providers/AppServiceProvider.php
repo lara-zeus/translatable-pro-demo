d32b04cf3e2fde28fb424067c6384c2e63fc7e4f
Ash Monsh <wh7r.com@gmail.com>
2025-12-17 21:55:34 +0300

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -31,12 +31,12 @@ class AppServiceProvider extends ServiceProvider
         Model::unguard();
 
         Blade::directive('zeus', function () {
-            return '<link rel="stylesheet" href="https://still-code.com/css/still-sign.css"><span class="still-font-koho still-group"><span class="still-font-koho still-font-semibold still-text-zprimary-500 group-hover:still-text-zsecondary-500 still-transition still-ease-in-out still-duration-300">Lara&nbsp;<span class="still-font-koho still-line-through still-italic still-text-zsecondary-500 group-hover:still-text-zprimary-500 still-transition still-ease-in-out still-duration-300">Z</span>eus</span></span>';
+            return 'zeus';
         });
 
         Blade::directive('stillStats', function ($code) {
             if (! app()->isLocal()) {
-                return '<!-- stats --><script async defer data-website-id="'.$code.'" src="https://stats.still-code.com/script.js"></script>';
+                return '<!-- stats --><script async defer data-website-id="'.$code.'" src="https://stats.larazeus.com/script.js"></script>';
             }
 
             return '<!-- no tags for you -->';
