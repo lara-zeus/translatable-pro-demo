9f411b6a38e5fbb99b667075970f3bb4dd45d260
a7medKhalid <ahmed1alghamdi2000@gmail.com>
2026-06-19 05:26:05 +0300

    add .gitignore to exclude all files except itself and add .DS_Store

--- storage/framework/views/0230f60f8fc6440b8db75022937f468a.php
+++
@@ -1,5 +0,0 @@
-<div <?php echo e($attributes->class(['fi-dropdown-list'])); ?>>
-    <?php echo e($slot); ?>
-
-</div>
-<?php /**PATH /Users/ahmedkhalid/Documents/laravel/larazeus/translatable-pro-demo/vendor/filament/support/resources/views/components/dropdown/list/index.blade.php ENDPATH**/ ?>
--- storage/framework/views/372c8e2cec18ba6f66b0ded4033bbb35.php
+++
@@ -1,2 +0,0 @@
-<div></div>
-<?php /**PATH /Users/ahmedkhalid/Documents/laravel/larazeus/translatable-pro-demo/resources/views/filament/hooks/topbar.blade.php ENDPATH**/ ?>
